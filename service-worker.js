d593899786a9658c0b18d45c8cc15b21d5e43210
Chris Roberts <chriroberts@expedia.com>
2015-06-24 06:42:41 +1000

    implement offline fallback

--- service-worker.js
+++ service-worker.js
@@ -9,9 +9,9 @@ if( 'function' === typeof importScripts) {
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('wdyk-demo-v1').then(function (cache) {
+        caches.open('web-directions-code-2015').then(function (cache) {
             return cache.addAll([
-                '/',
+                '/offline.html',
                 'styles.css',
                 'expedia.com.au.png',
                 'fourpoints.jpg'
@@ -26,13 +26,20 @@ self.addEventListener('fetch', function (event) {
     var requestUrl = new URL(event.request.url).pathname;
     console.log('Requested:', requestUrl);
 
-    event.respondWith(caches.match(event.request).then(function (response) {
-            // CACHE
+    event.respondWith(
+
+        // search cache
+        caches.match(event.request).then(function (response) {
+            // Assets from cache
             if (response) {
                 return response;
             }
-            // NETWORK
-            return fetch(event.request);
+            // Get itinerary from network
+            // or fallback to cache
+            return fetch(event.request).catch(function() {
+                console.log('cannot connect');
+                return caches.match('/offline.html');
+            });
         })
     );
 });
