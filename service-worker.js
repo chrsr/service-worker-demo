9e5aa9df43ff75c9999642d1072a07f7697284ca
Chris Roberts <chriroberts@expedia.com>
2015-06-24 17:15:42 +1000

    network fallback working

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,13 @@ if( 'function' === typeof importScripts) {
     importScripts('cache-polyfill.js');
 }
 
+var cacheKey = 'ws-code-2015-v2';
+
 // INSTALL
 
 self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open('web-directions-code-2015').then(function (cache) {
+        caches.open(cacheKey).then(function (cache) {
             return cache.addAll([
                 '/offline.html',
                 'styles.css',
@@ -27,25 +29,20 @@ self.addEventListener('fetch', function (event) {
     console.log('Requested:', requestUrl);
 
     event.respondWith(
-        caches.open('ws-code-2015-v1').then(function (cache) {
-
-            // search cache
-            cache.match(event.request).then(function (response) {
-                // First, serve from cache.
-                if (response) {
-                    return response;
-                }
-                // Attempt to get itinerary from network
-                // or fallback to cache
-                return fetch(event.request).then(function (response) {
-                    // if network
-                    // update offline.htm with response
-                    cache.put('/offline.html', response.clone());
-                    return response;
-                }).catch(function() {
-                    console.log('cannot connect');
-                    return caches.match('/offline.html');
-                });
+        caches.match(event.request).then(function (response) {
+            // found cache
+            if (response) {
+                return response;
+            }
+            // network with fallback
+            return fetch(event.request).then(function (response) {
+                // try the network
+                // for non-cached requests
+                return response;
+            }).catch(function() {
+                // no connection
+                // fallback to message or previous versions?
+                return caches.match('/offline.html');
             });
         })
     );
